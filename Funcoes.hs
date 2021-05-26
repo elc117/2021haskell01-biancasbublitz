047304c8f7a7ec6194183cbd017f90bb18e3c214
Bianca Sabrina Sabrina <>
2021-05-26 13:27:24 +0000

    realização das atividades da parte 2

--- Funcoes.hs
+++ Funcoes.hs
@@ -5,3 +5,25 @@ htmlItem x = "<li>" ++ x ++ "</li>"
 
 isVowel2 :: Char -> Bool
 isVowel2 x = elem x "aAeEiIoOuU"
+
+isElderly :: Int -> Bool
+isElderly idade 
+  | idade >= 65 = True
+  | idade < 65 = False
+
+isLongWord :: String -> Bool
+isLongWord s = length s > 10
+
+isEvenBetter  :: Int -> Bool
+isEvenBetter n = mod n 2 == 0
+
+between60and80 :: Int -> Bool
+between60and80 x = x > 60 && x < 80
+
+achaEspaco :: Char -> Int
+achaEspaco x 
+  | x == ' '  = 1
+  | otherwise = 0
+
+circleArea :: Float -> Float
+circleArea r = pi * (r^2)
--- haskell01parte2.hs
+++ haskell01parte2.hs
@@ -7,8 +7,30 @@ import Funcoes
 itemize :: [String] -> [String]
 itemize x = map htmlItem x
 
-
-
 -- 02
---onlyVowels :: String -> String
---onlyVowels filter isVowel2
+onlyVowels :: String -> String
+onlyVowels x = filter isVowel2 x
+
+-- 03
+onlyElderly :: [Int] -> [Int]
+onlyElderly x = filter isElderly x
+
+-- 04
+onlyLongWords :: [String] -> [String]
+onlyLongWords x = filter isLongWord x
+
+-- 05
+onlyEven :: [Int] -> [Int]
+onlyEven x = filter isEvenBetter x
+
+-- 06
+onlyBetween60and80 :: [Int] -> [Int]
+onlyBetween60and80 x = filter between60and80 x
+
+-- 07
+countSpaces :: String -> Int
+countSpaces x = sum (map achaEspaco x)
+
+-- 08
+calcAreas :: [Float] -> [Float]
+calcAreas x = map circleArea x
